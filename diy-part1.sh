fd8529fb24b91cc16dfbb0d56dc2500e23fad7f5
XueLongYan <66116320+XueLongYan@users.noreply.github.com>
2024-02-25 16:29:16 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -19,3 +19,4 @@ echo 'src-git lienol https://github.com/Lienol/openwrt-package' >>feeds.conf.def
 #echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
 echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+echo 'src-git smartdns https://github.com/pymumu/smartdns' >>feeds.conf.default
