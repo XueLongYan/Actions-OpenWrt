36dd8067406d10ec9109aa9e56ca88f53ecb01ce
XueLongYan <66116320+XueLongYan@users.noreply.github.com>
2024-04-30 16:47:29 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -19,3 +19,4 @@ echo 'src-git helloworld https://github.com/XueLongYan/helloworld' >>feeds.conf.
 #echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
 echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+echo 'src-git netspeedtest https://github.com/sirpdboy/netspeedtest' >>feeds.conf.default
