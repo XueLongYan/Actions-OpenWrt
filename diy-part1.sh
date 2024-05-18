6d9501b2740c9e4ed9aa5f60ae56d34ea2503288
XueLongYan <66116320+XueLongYan@users.noreply.github.com>
2024-05-19 05:34:07 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,11 +14,9 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-echo 'src-git helloworld https://github.com/XueLongYan/helloworld' >>feeds.conf.default
+#echo 'src-git helloworld https://github.com/XueLongYan/helloworld' >>feeds.conf.default
 #echo 'src-git lienol https://github.com/Lienol/openwrt-package' >>feeds.conf.default
 echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
 echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 echo 'src-git netspeedtest https://github.com/sirpdboy/netspeedtest' >>feeds.conf.default
-#echo 'src-git jellyfin https://github.com/jellyfin/jellyfin' >>feeds.conf.default
-#echo 'src-git ffmpeg https://github.com/jellyfin/jellyfin-ffmpeg' >>feeds.conf.default
