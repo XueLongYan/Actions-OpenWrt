ecb10ad1007cec5488424ca6b57efc7e49f43427
XueLongYan <66116320+XueLongYan@users.noreply.github.com>
2024-05-18 17:30:50 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -20,5 +20,5 @@ echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.de
 echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 echo 'src-git netspeedtest https://github.com/sirpdboy/netspeedtest' >>feeds.conf.default
-echo 'src-git jellyfin https://github.com/jellyfin/jellyfin' >>feeds.conf.default
-echo 'src-git ffmpeg https://github.com/jellyfin/jellyfin-ffmpeg' >>feeds.conf.default
+#echo 'src-git jellyfin https://github.com/jellyfin/jellyfin' >>feeds.conf.default
+#echo 'src-git ffmpeg https://github.com/jellyfin/jellyfin-ffmpeg' >>feeds.conf.default
