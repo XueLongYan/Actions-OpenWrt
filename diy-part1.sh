0609ab3658133446e15f837292892dac04f63a7b
XueLongYan <66116320+XueLongYan@users.noreply.github.com>
2024-08-19 20:02:31 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,8 +15,8 @@
 
 # Add a feed source
 echo 'src-git helloworld https://github.com/XueLongYan/helloworld' >>feeds.conf.default
-echo 'src-git lienol https://github.com/Lienol/openwrt-package' >>feeds.conf.default
-echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
+#echo 'src-git lienol https://github.com/Lienol/openwrt-package' >>feeds.conf.default
+#echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
 echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-echo 'src-git netspeedtest https://github.com/sirpdboy/netspeedtest' >>feeds.conf.default
+#echo 'src-git netspeedtest https://github.com/sirpdboy/netspeedtest' >>feeds.conf.default
