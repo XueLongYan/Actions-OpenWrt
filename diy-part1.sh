67a984386603ba0997272fa7ecce4f7357b5cc79
XueLongYan <66116320+XueLongYan@users.noreply.github.com>
2025-04-20 13:41:27 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -20,3 +20,4 @@
 #echo "src-git small https://github.com/kenzok8/small-package.git" >> "feeds.conf.default"
 echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git" >> "feeds.conf.default"
 echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git" >> "feeds.conf.default"
+echo "src-git sirpdboy https://github.com/sirpdboy/sirpdboy-package.git >> "feeds.conf.default"
