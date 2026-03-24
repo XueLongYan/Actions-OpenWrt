6823b2db30fcce19a0e05721266f2afee80f34dd
XueLongYan <66116320+XueLongYan@users.noreply.github.com>
2026-03-24 23:02:51 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -23,5 +23,5 @@ echo "src-git passwall https://github.com/Openwrt-Passwall/openwrt-passwall.git"
 #echo "src-git openclash https://github.com/vernesong/OpenClash.git" >> "feeds.conf.default"
 #echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
 #echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> "feeds.conf.default"
-echo "src-git dae https://github.com/QiuSimons/luci-app-daed" >> "feeds.conf.default"
+#echo "src-git dae https://github.com/QiuSimons/luci-app-daed" >> "feeds.conf.default"
 #git clone https://github.com/xRetia/luci-app-usbip-server.git ./package/luci-app-usbip-server
